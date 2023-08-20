0feca764592d9e35476c433d09fd02f97791d686
Jonviter Simbolon <dolijonviter17@gmail.com>
2023-08-20 19:02:50 +0700

    Delete src/state directory

--- src/state/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './store';
-export * from './reducers';
-export * as repositoriesCreators from './action-creators/repositoriesCreators';
-export * as livescoresCreators from './action-creators/livescoresCreators';
